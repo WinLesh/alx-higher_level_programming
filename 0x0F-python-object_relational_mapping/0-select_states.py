9d80e4f2b028171933b1e60054091b5af9fca867
WinLesh <l.seshabo@gmail.com>
2023-04-17 14:55:59 -0700

    Work

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,4 +3,4 @@
 
 import MySQLdb;
 
-conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd = "root", db="My_db", charset="utfs";
+conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd = "root", db="My_db", charset="utfs")
