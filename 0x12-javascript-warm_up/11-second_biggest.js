3307207c0622e5316cf99f64864a266b7245cf80
WinLesh <l.seshabo@gmail.com>
2023-04-17 06:03:35 -0700

    Second biggets

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,6 @@ if (process.argv.length <= 3) {
   console.log('0');
 } else {
   const arr = process.argv.slice(2).map(Number);
-  const sbiggest = arr.sort(function (l, s) { return l - s; })[1];
+  const sbiggest = arr.sort(function (l, s) { return s - l; })[1];
   console.log(sbiggest);
 }
