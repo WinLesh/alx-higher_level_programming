1f41901d89105cedc899595e92b12e57a1fc0ec8
WinLesh <l.seshabo@gmail.com>
2023-04-21 02:02:02 -0700

    Filte States

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,7 +7,7 @@ import sys
 
 
 if __name__ == "__main__":
-    connec = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    connec = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = connec.cursor()
     cur.execute("SELECT * FROM states")
     values = cur.fetchall()
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
-"""" lists all states from the database hbtn_0e_0_usa starting with N """
-
+"""" Script that lists all states from the database hbtn_0e_0_usa starting with N """
 import MySQLdb
 import sys
 
