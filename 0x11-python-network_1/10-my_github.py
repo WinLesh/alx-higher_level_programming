b2438bfeacb1ceda31e52bd478302d2d9ddfb74f
WinLesh <l.seshabo@gmail.com>
2023-05-02 03:00:16 -0700

    i will make it

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -2,8 +2,15 @@
 """ A Script that gets info of account from Github API using credentials
 uses the github API to diplay your id
 """
-
 if __name__ == "__main__":
-    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
-    r = requests.get("https://api.github.com/user", auth=auth)
-    print(r.json().get("id"))
+    import sys
+    import requests
+    user = sys.argv[1]
+    pas = sys.argv[2]
+    API = "https://api.github.com/users"
+    API = API + "/{}".format(user)
+    r = requests.get(API, auth=requests.auth.HTTPBasicAuth(user, pas))
+    if r.status_code == 200:
+        print(r.json().get('id'))
+    else:
+        print("None")if __name__ == "__main__":
